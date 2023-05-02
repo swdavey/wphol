0640cafa39da8becf5c3eb120c6a4ca1fd5282e3
swdavey <55132360+swdavey@users.noreply.github.com>
2023-05-02 06:31:00 +0100

    Delete provider.tf

--- provider.tf
+++
@@ -1,7 +0,0 @@
-provider "oci" {
-  tenancy_ocid = var.tenancy_ocid
-  region = var.region
-  user_ocid = var.user_ocid
-  fingerprint = var.fingerprint
-  private_key_path = var.private_key_path
-}
